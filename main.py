4c9c9470d44e3c4b55e93b8e3977ad064b077cb4
Kenneth <kenneth_wj@outlook.com>
2025-03-11 23:21:13 +0800

    updated main to work with admin features

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 
-from src.tele_common import start, help_command, menu_command, button_callback, handle_message
+from src.database import init_user_mgr
+from src.tele_common import start, help_command, menu_command, common_callback, handle_message
+from src.tele_admin import admin_command, admin_callback, add_premium_conv, add_credits_conv
+
+from config import (
+    QUERY_PATH,
+    DB_MASTER_FPATH,
+)
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,6 +22,7 @@ logger = logging.getLogger(__name__)
 def start_bot() -> None:
     # Load Variable
     load_dotenv()
+    init_user_mgr(DB_MASTER_FPATH, QUERY_PATH)
 
     TELE_TOKEN: str | None = os.getenv("TELE_API_KEY")
     if not TELE_TOKEN:
@@ -25,9 +33,14 @@ def start_bot() -> None:
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-
+    application.add_handler(CommandHandler("admin", admin_command))
     application.add_handler(CommandHandler("change_model", menu_command))
-    application.add_handler(CallbackQueryHandler(button_callback))
+
+    application.add_handler(add_premium_conv)
+    application.add_handler(add_credits_conv)
+
+    application.add_handler(CallbackQueryHandler(common_callback, pattern="^(provider_|model_|back_)"))
+    application.add_handler(CallbackQueryHandler(admin_callback, pattern="^admin_"))
 
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
